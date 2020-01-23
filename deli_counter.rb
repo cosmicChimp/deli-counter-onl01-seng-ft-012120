0a69490d26ca3b67e59b2ad87d79650818d56ad2
Ian Marrs <ian.t.marrs@gmail.com>
2020-01-23 20:50:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,13 @@
 katz_deli = []
 
 def line(katz_deli)
-  positions = [];
+  line_positions = [];
   if katz_deli.empty?
     puts "The line is currently empty."
   else
     number = 1
     katz_deli.each do |customer|
-      positions << "#{number}. #{customer}"
+      line_positions << "#{number}. #{customer}"
       number += 1
     end
     puts "The line is currently: #{line_positions.join(" ")}"
